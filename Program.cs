21978dc3ffff75380c72453d64b8ce7517f6f218
Adam Viera <adamjviera@gmail.com>
2019-09-19 15:41:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Methods_Deliv3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your First and Last name: ");
+            Console.WriteLine("");
+            Console.Write("Example: 'John Smith' ");
+            Console.WriteLine("");
+            Console.WriteLine("_____________________________________________________");
+            try
+            {
+                //This variable will gather data from user input
+                string input = Console.ReadLine();
+                //Execute void method
+                user(input);                
+            }
+            catch
+            {
+                Console.WriteLine("There was an error, please enter valid characters");
+                Console.ReadKey(true);
+            } //End of catch
+        }
+
+        private static void user(string input)
+        {
+            Console.WriteLine("_____________________________________________________");
+            Console.WriteLine("Hello " + input + "!");
+            Console.ReadKey(true);
+
+        }
+    }
+}
